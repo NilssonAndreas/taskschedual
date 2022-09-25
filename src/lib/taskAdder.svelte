f642a7a6d1a082691d18ca237b9a03c02bdb67ca
Andreas <annt21@student.bth.se>
2022-09-25 08:50:27 +0100

    Validation on task-form

--- src/lib/taskAdder.svelte
+++ src/lib/taskAdder.svelte
@@ -4,11 +4,14 @@
   import { formatDate } from "./HelperFunc/time";
   import { Label, Input, Select, Button, Helper } from "flowbite-svelte";
 
+  // Sets MinTime in form to todays date.
   let minTime = formatDate(new Date());
-  let status;
   let description = null;
   let startTime = minTime;
   let deadline = null;
+  let estimatedDuration;
+
+  // CURRENT CATEGORYS ( SHOULD LATER BE ADDED AS TABLE IN DB )
   let category = [
     { value: "Work", name: "Work" },
     { value: "School", name: "School" },
@@ -16,10 +19,19 @@
     { value: "Kids", name: "Kids" },
     { value: "Sport", name: "Sport" },
   ];
+
+  // Keeps track of invalid values in form
+  let validationErrors = {
+    description: false,
+    startDate: false,
+    deadline: false,
+    estimatedDuration: false,
+    selected: false,
+  };
+
+  // Value if category is selected
   let selected = null;
   const taskStatus = "Not started";
-  let estimatedDuration;
-  let visible = false;
 
   // Clear all tasks
   async function clearTasks() {
@@ -43,30 +55,65 @@
         status: taskStatus,
       });
 
-      status = `Task ${description} successfully added. Got id ${id}`;
       description = "";
-      startTime = null;
+      startTime = minTime;
       deadline = null;
       estimatedDuration = null;
     } catch (error) {
-      status = `Failed to add ${description}: ${error}`;
+      console.log("Error while adding task")
+    }
+  }
+
+  //Takes array of strings
+  // Sets value to true in validationError array
+  function setValidationError(errorArray) {
+    errorArray.forEach((name) => {
+      validationErrors[name] = true;
+    });
+  }
+
+  // Sets all values to false in ValidationError array
+  function resetValidationError() {
+    for (let [key, value] of Object.entries(validationErrors)) {
+      validationErrors[key] = false;
     }
   }
 
   //Validate data from task form
-  async function validateTask(description, startTime, deadline) {
-    if (
-      description == null ||
-      description == "" ||
-      startTime == null ||
-      deadline == null ||
-      category == null ||
-      estimatedDuration <= 0
-    ) {
-      visible = true;
-    } else {
+  function validateTask() {
+    resetValidationError();
+    let isValid = true;
+    let notValidarray = [];
+    if (description == null || description == "") {
+      isValid = false;
+      notValidarray.push("description");
+    }
+
+    if (startTime == null) {
+      isValid = false;
+      notValidarray.push("startTime");
+    }
+
+    if (selected == null) {
+      isValid = false;
+      notValidarray.push("selected");
+    }
+
+    if (deadline == null || deadline < startTime) {
+      isValid = false;
+      notValidarray.push("deadline");
+    }
+
+    if (estimatedDuration <= 0) {
+      isValid = false;
+      notValidarray.push("estimatedDuration");
+    }
+
+    if (isValid == true) {
+      resetValidationError();
       addTask();
-      visible = false;
+    } else {
+      setValidationError(notValidarray);
     }
   }
 </script>
@@ -80,6 +127,11 @@
       placeholder="Enter description"
       bind:value={description}
     />
+    {#if validationErrors["description"]}
+      <Helper class="mt-1" color="red"
+        ><span class="font-bold text-sm">Error!</span> Please enter a Description</Helper
+      >
+    {/if}
   </div>
 
   <!-- Category -->
@@ -88,6 +140,11 @@
       >Select category
       <Select class="mt-2" items={category} bind:value={selected} />
     </Label>
+    {#if validationErrors["selected"]}
+      <Helper class="mt-1" color="red"
+        ><span class="font-bold text-sm">Error!</span> Choose category</Helper
+      >
+    {/if}
   </div>
 
   <!-- Starting Time -->
@@ -96,6 +153,11 @@
       >Select Start time
       <Input id="StartTime" type="date" min={minTime} bind:value={startTime} />
     </Label>
+    {#if validationErrors["startTime"]}
+      <Helper class="mt-1" color="red"
+        ><span class="font-bold text-sm">Error!</span> Enter Start time</Helper
+      >
+    {/if}
   </div>
   <!-- Deadline -->
   <div class="mt-6">
@@ -103,6 +165,11 @@
       >Select deadline
       <Input id="deadline" type="date" min={startTime} bind:value={deadline} />
     </Label>
+    {#if validationErrors["deadline"]}
+      <Helper class="mt-1" color="red"
+        ><span class="font-bold text-sm">Error!</span> Enter correct deadline</Helper
+      >
+    {/if}
   </div>
 
   <!-- Estimated duration -->
@@ -117,24 +184,17 @@
       min="0"
       bind:value={estimatedDuration}
     />
+    {#if validationErrors["estimatedDuration"]}
+      <Helper class="mt-1" color="red"
+        ><span class="font-bold text-sm">Error!</span> Enter a Estimated duration</Helper
+      >
+    {/if}
   </div>
 
   <!-- Button for submit -->
   <div class="mt-6 col-span-2 ">
-    <Button
-      gradient
-      color="purple"
-      size="xl"
-      on:click={() => validateTask(description, startTime, deadline)}
+    <Button gradient color="purple" size="xl" on:click={() => validateTask()}
       >Add task
     </Button>
   </div>
-
-  {#if visible}
-    <div class="mt-6 col-span-2 ">
-      <Helper class="mt-2" color="red"
-        ><span class="font-bold text-xl">Error!</span> Some value is missing.</Helper
-      >
-    </div>
-  {/if}
 </div>
